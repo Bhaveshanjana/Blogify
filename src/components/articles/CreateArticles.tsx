1cb29afae9c4ec6c3cfe87f271ab63a671c8cf4e
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-06-14 09:55:06 +0530

    add article page

--- src/components/articles/CreateArticles.tsx
+++ src/components/articles/CreateArticles.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import dynamic from "next/dynamic";
+import "react-quill-new/dist/quill.snow.css";
+
+const ReactQuil = dynamic(() => import("react-quill-new"), { ssr: false });
+import { Button } from "@/components/ui/button";
 
 const CreateArticles = () => {
+  const [content, setContent] = useState("");
   return (
     <div className="max-w-4xl mx-auto p-6">
       <Card>
@@ -37,12 +45,11 @@ const CreateArticles = () => {
               </div>
               <div className="space-y-2">
                 <Label>Featured Image </Label>
-                <Input
-                  type="file"
-                  id="featuredImage"
-                  name="featuredImage"
-                  accept="image/*"
-                />
+                <ReactQuil theme="snow" value={content} onChange={setContent} />
+              </div>
+              <div className="flex justify-end gap-4">
+                <Button variant={"outline"}>Cancel</Button>
+                <Button type="submit">Publish Article</Button>
               </div>
             </div>
           </form>
